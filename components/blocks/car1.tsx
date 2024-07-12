6ddfa19479a15dd29046d4b481a472f38b31209c
beni20 <3387957@users.noreply.github.com>
2024-07-12 10:40:12 +0200

    .

---
+++ components/blocks/car1.tsx
@@ -0,0 +1,56 @@
+
+
+import React from "react";
+import { Container } from "../util/container";
+import { Section } from "../util/section";
+import { TinaMarkdown } from "tinacms/dist/rich-text";
+import type { TinaTemplate } from "tinacms";
+import { PageBlocksCar1 } from "../../tina/__generated__/types";
+import { tinaField } from "tinacms/dist/react";
+import { Button } from "flowbite-react";
+import { Carousel } from "flowbite-react";
+
+
+
+export const Car1 = ({ data }: { data: PageBlocksCar1 }) => {
+  return (
+    <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+      <Carousel pauseOnHover>
+        <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+      </Carousel>
+    </div>
+  );
+};
+
+export const car1BlockSchema: TinaTemplate = {
+  name: "car1",
+  label: "Car1",
+  ui: {
+    previewSrc: "/blocks/content.png",
+    defaultItem: {
+      body: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.",
+    },
+  },
+  fields: [
+    {
+      type: "rich-text",
+      label: "Body",
+      name: "body",
+    },
+    {
+      type: "string",
+      label: "Color",
+      name: "color",
+      options: [
+        { label: "Default", value: "default" },
+        { label: "Tint", value: "tint" },
+        { label: "Primary", value: "primary" },
+      ],
+    },
+  ],
+};
+
